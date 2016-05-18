6eb683ce7d3e8b8b1f7d4b2301d0d4ef0e424b80
a-tagai <tagai1106@gmail.com>
2016-05-18 10:42:22 +0900

    テストコードの実装中

--- spec/controllers/users/sessions_controller_spec.rb
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -1,12 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Users::SessionsController, :type => :controller do
+  before do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+
   describe 'GET sessions/#new' do
-    login_user
-    it "should have a current_user" do
-      skip 'TODO:後で実装'
-      # note the fact that you should remove the "validate_session" parameter if this was a scaffold-generated controller
-      expect(controller.current_user).to_not eq(nil)
+    context '一般ユーザーでのログイン状態の場合' do
+      login_user
+      before(:each) do
+        get :new
+      end
+
+      example "current_userでログインユーザー情報が取得できること" do
+        expect(controller.current_user).to_not eq(nil)
+      end
+
+      example "ルートページへリダイレクトされること" do
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context '未ログイン状態の場合' do
+      before(:each) do
+        skip 'Devise.mappings[:user]を指定して良いのか後で調べる'
+        get :new
+      end
+
+      example "current_userでログインユーザー情報が取得できないこと" do
+        expect(controller.current_user).to eq(nil)
+      end
+
+      example ":newテンプレートがレンダリングされていること" do
+        expect(response).to render_template :new
+      end
     end
   end
 end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,6 +2,8 @@
 module ControllerMacros
   def login_user
     before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+
       user = FactoryGirl.create(:ichiro)
       user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
       sign_in user
